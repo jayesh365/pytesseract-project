5c486dab6fd92a5f14c1c0919ae413fde13060ac
jayesh365 <jayesh.khullar@sickkids.ca>
2019-05-10 09:02:53 -0400

    updated ranges

--- ocr_server/ocr_core.py
+++ ocr_server/ocr_core.py
@@ -17,19 +17,15 @@ def ocr_core(imgPath):
     # Pillow will be used to open the image, and PyTesseract
     # will be used to parse strings from the image
     parsedText = pytesseract.image_to_string(Image.open(imgPath))
-    parsedTextD = pytesseract.image_to_data(Image.open(imgPath))
-    print(parsedTextD)
-    # parsedTextLocation = pytesseract.image_to_boxes(Image.open(imgPath))
+    # parsedTextD = pytesseract.image_to_data(Image.open(imgPath))
+    # print(parsedTextD)
+    parsedTextLocation = pytesseract.image_to_data(Image.open(imgPath))
+    print(parsedTextLocation)
     # put all parsed text into an array
     parsedTextArr = [i for i in parsedText]
-    print(parsedTextArr)
 
     # traverse the parsed text to find the special char to start openCV
     for j in range(len(parsedTextArr)):
         if parsedTextArr[j] == 'x' and parsedTextArr[j-1] == 'x':
             locateCheckBoxes(imgPath)
     return parsedText
-
-
-if __name__ == '__main__':
-    ocr_core('images/check.PNG')
--- ocr_server/openCV_core.py
+++ ocr_server/openCV_core.py
@@ -4,8 +4,9 @@ import cv2
 
 def locateCheckBoxes(image):
     '''
+    (string) -> null
     this function will take in an image of a form, and find all of
-    the checkboxes.
+    the checkboxes and call locateAnswers()
     '''
     # find countours of a binary image
     # read image
@@ -17,30 +18,33 @@ def locateCheckBoxes(image):
     # contour approximation
     contours, hierarchy = cv2.findContours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(im, contours, -1, (0,255,0), 3)
     # find all boxes
-    # cnt = 581
     for cnt in contours:
-        if cv2.contourArea(cnt) > 382 and cv2.contourArea(cnt) < 1079:
+        if cv2.contourArea(cnt):
             # draw contour around box
             x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 3)
-            print(x, y, w, h)
-    # find the answers
-    locateAnswers(image)
+            if x > 995 and w > 35 and h > 14:
+                cv2.rectangle(im, (x, y), (x+w, y+h), (255, 0, 0), 3)
+    cv2.imshow('Contour Pic', im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 def locateAnswers(image):
+    '''
+    (string) -> null
+    this function will traverse all checkboxes and find the answers.
+    '''
     im = cv2.imread(image)
     imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 128, 255, 0)
     contours, hierarchy = cv2.findContours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-        if cv2.contourArea(cnt) < 300:
+        if cv2.contourArea(cnt) > 100 and cv2.contourArea(cnt) < 300:
             # draw contour around box
             x, y, w, h = cv2.boundingRect(cnt)
-            if x > 1150 and y >= 4 and x < 1450:
+            if x > 1150 and x < 1460:
                 cv2.rectangle(im, (x, y), (x+w, y+h), (0, 0, 255), 3)
     cv2.imshow('Contour Pic', im)
     cv2.waitKey(0)
